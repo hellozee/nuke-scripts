ca95a0cfa309c5186ca2657ab8f8d722103122d7
Kuntal Majumder <hellozee@disroot.org>
2018-12-09 22:58:48 +0530

    Multiple changes to marvel.py  - moved everything to functions  - images are now resized according to the project setting  - the motion blur node is selected after running the script

--- marvel.py
+++ marvel.py
@@ -6,40 +6,47 @@ if length < 2 :
     nuke.warning("At least 2 images required to create the effect")
     nuke.message("At least 2 images required to create the effect")
 
-i = 0
-read1 = nuke.nodes.Read(file=images[i],auto_alpha=True,premultiplied=True)
-transform1 = nuke.nodes.Transform()
-transform1['translate'].setAnimated()
-transform1['translate'].setValue([0,1080],time=1)
-transform1['translate'].setValue([0,0],time=3)
-transform1.setInput(0,read1)
-i += 1
-
-read2 = nuke.nodes.Read(file=images[i],auto_alpha=True,premultiplied=True)
-transform2 = nuke.nodes.Transform()
-transform2['translate'].setAnimated()
-transform2['translate'].setValue([0,1080],time=1)
-transform2['translate'].setValue([0,0],time=3)
-transform2.setInput(0,read2)
-timeoffset1 = nuke.nodes.TimeOffset()
-timeoffset1['time_offset'].setValue(i*4)
-timeoffset1.setInput(0,transform2)
-i += 1
-
-merge = nuke.nodes.Merge(inputs=[transform1, timeoffset1])
-
-while i < length:
-    read = nuke.nodes.Read(file=images[i],auto_alpha=True,premultiplied=True)
+def get_transform(height):
     transform = nuke.nodes.Transform()
     transform['translate'].setAnimated()
-    transform['translate'].setValue([0,1080],time=1)
+    transform['translate'].setValue([0,height],time=1)
     transform['translate'].setValue([0,0],time=3)
-    transform.setInput(0,read)
+    return transform
+
+def get_reformat():
+    reformat = nuke.nodes.Reformat()
+    reformat['resize'].setValue('fill')
+    return reformat
+
+def get_timeoffset(pos):
     timeoffset = nuke.nodes.TimeOffset()
-    timeoffset['time_offset'].setValue(i*4)
+    timeoffset['time_offset'].setValue(pos*4)
+    return timeoffset
+
+def manipulate_node(filename,pos):
+    read = nuke.nodes.Read(file=images[i],auto_alpha=True,premultiplied=True)
+    reformat = get_reformat()
+    reformat.setInput(0,read)
+    crop = nuke.nodes.Crop()
+    crop.setInput(0,reformat)
+    transform = get_transform(read['format'].value().height())
+    transform.setInput(0,crop)
+    timeoffset = get_timeoffset(pos)
     timeoffset.setInput(0,transform)
-    merge = nuke.nodes.Merge(inputs=[merge, timeoffset])
+    return timeoffset
+    
+i = 0
+node1 = manipulate_node(images[i],i)
+i += 1
+node2 = manipulate_node(images[i],i)
+i += 1
+merge = nuke.nodes.Merge(inputs=[node1, node2])
+
+while i < length:
+    node = manipulate_node(images[i],i)
+    merge = nuke.nodes.Merge(inputs=[merge, node])
     i += 1
 
 motionblur = nuke.nodes.MotionBlur()
 motionblur.setInput(0,merge)
+motionblur.setSelected(True)
